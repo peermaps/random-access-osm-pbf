a3c6583f9ee6b1695f93bbc4e92061c4e407412f
Marina Kukso <marina.kukso@gmail.com>
2019-06-26 10:51:57 -1000

    added a few more tests

--- test/parse.js
+++ test/parse.js
@@ -24,8 +24,8 @@ test('parse start unaligned', function (t) {
   t.plan(5)
   var store = raf(path.resolve(__dirname, 'fixtures/kharkiv_ukraine.osm.pbf'))
   var osm = raOSM({
-    start: 1042608,
-    end: 1345615,
+    start: 1042608, //unaligned
+    end: 1345615, //aligned
     size: 8212140,
     read: store.read.bind(store)
   })
@@ -40,8 +40,8 @@ test('parse start unaligned forward', function (t) {
   t.plan(4)
   var store = raf(path.resolve(__dirname, 'fixtures/kharkiv_ukraine.osm.pbf'))
   var osm = raOSM({
-    start: 1042709,
-    end: 1345615,
+    start: 1042709, //unaligned
+    end: 1345615, //aligned
     size: 8212140,
     read: store.read.bind(store)
   })
@@ -71,6 +71,21 @@ test('parse whole file', function (t) {
     8000, 8000, 8000, 8000, 8000, 8000, 8000, 8000, 1211, 8000, 8000, 8000,
     8000, 8000, 8000, 8000, 8000, 8000, 8000, 8000, 8000, 8000, 8000, 8000,
     8000, 7338, 3468]
+  osm.pipe(through.obj(function write (items, enc, next) {
+    t.equals(items.length, expectedLength.shift())
+    next()
+  }))
+})
+test('parse end unaligned', function (t) {
+  t.plan(5)
+  var store = raf(path.resolve(__dirname, 'fixtures/kharkiv_ukraine.osm.pbf'))
+  var osm = raOSM({
+    start: 1042608, //aligned
+    end: 1345610, //aligned
+    size: 8212140,
+    read: store.read.bind(store)
+  })
+  var expectedLength = [8000, 8000, 8000, 8000, 8000]
   osm.pipe(through.obj(function write (items, enc, next) {
     t.equals(items.length, expectedLength.shift())
     next()
